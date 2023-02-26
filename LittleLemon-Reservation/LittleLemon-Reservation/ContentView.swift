fc4c04733481dbd3e5d7d1b267db1d6ed9ff7ca7
Mert Urhan <mert-urhan@hotmail.com>
2023-02-26 20:04:41 +0300

    First Commit

---
+++ LittleLemon-Reservation/LittleLemon-Reservation/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LittleLemon-Reservation
+//
+//  Created by Mert Urhan on 26.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Litte Lemon Reservation App")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LittleLemon-Reservation/LittleLemon-Reservation/LittleLemon_ReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemon_ReservationApp.swift
+//  LittleLemon-Reservation
+//
+//  Created by Mert Urhan on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemon_ReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
